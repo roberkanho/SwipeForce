d06cf22459a97294ddae29d59c073dfddcc18305
Carlos Cheung <cc653@georgetown.edu>
2015-06-13 13:01:29 -0700

    blank slate

---
+++ swipeforce/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  swipeforce
+//
+//  Created by Carlos Cheung on 6/13/15.
+//  Copyright (c) 2015 carloscheung. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ swipeforceTests/swipeforceTests.swift
@@ -0,0 +1,36 @@
+//
+//  swipeforceTests.swift
+//  swipeforceTests
+//
+//  Created by Carlos Cheung on 6/13/15.
+//  Copyright (c) 2015 carloscheung. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class swipeforceTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
